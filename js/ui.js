b6c46bb4dc667b220e66eac48ab5647d3c7a7c20
telagraphic <telagraphic@gmail.com>
2018-09-04 10:22:33 -0400

    breakpoint fixes

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,5 @@
-// var correlation_checkbox = document.querySelector("#correlation");
-// var correlation_table = document.querySelector(".correlationClass");
-// correlation_checkbox.addEventListener("click", function(event) {
-//   correlation_table.classList.toggle("show-correlation-table");
-// });
+var correlation_checkbox = document.querySelector("#correlation");
+var correlation_table = document.querySelector(".correlationClass");
+correlation_checkbox.addEventListener("click", function(event) {
+  correlation_table.classList.toggle("show-correlation-table");
+});
